fac845cc40431e464ce10c17adf5dd1b37560074
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 00:22:47 +0530

    Add files via upload

---
+++ Explicit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            long longValue = 1234567L;
+            int intValue = (int)longValue;
+            Console.WriteLine("(int) {0} = {1}", longValue, intValue);
+
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
