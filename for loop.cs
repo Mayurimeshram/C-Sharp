5843595dbd4688f9a1e1765dcef1c5a0c69ba065
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 00:24:06 +0530

    Add files via upload

---
+++ for loop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            int i;
+            for(i=1;i<10;i++)
+            {
+                Console.Write("i =" + i);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
