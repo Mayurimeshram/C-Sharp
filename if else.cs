74c2fb0063eda90f07733e272b2e61a57223cdc7
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 00:24:42 +0530

    Add files via upload

---
+++ if else.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Else
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            int n1, n2;
+            Console.WriteLine("enter 2 no.");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2)
+
+
+              
+
+      
