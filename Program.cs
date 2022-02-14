005040a09ca05906a8b5276d8f575e76c882baf6
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-14 23:51:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,31 +1,26 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Byte
+namespace character
 {
     class Program
     {
 
         public static void Main()
         {
-            byte b = 25; // unsigned byte for storing positive no.
-            Console.WriteLine("byte value " + b);
-            Console.WriteLine("enter byte");
-            b = Convert.ToByte(Console.ReadLine());
-            Console.WriteLine("byte = " + b);
-            //for negative as well as positive no.
-            sbyte b1 = -23; //signed byte
-            Console.WriteLine("sbyte value " + b1);
-            Console.WriteLine("enter sbyte");
-            b1 = Convert.ToSByte(Console.ReadLine());
-            Console.WriteLine("sbyte = " + b1);
+            char b = 'M';
+            Console.WriteLine(" value " + b);
+            Console.WriteLine("enter value ");
+            b = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("value = " + b);
+
 
 
             Console.ReadLine();
         }
     }
 }
+
