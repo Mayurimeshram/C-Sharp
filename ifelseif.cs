6a34790e97b5a3b6aa0004f4f40924d57945c6d0
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 00:27:36 +0530

    Add files via upload

---
+++ ifelseif.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            
+            int n1, n2, n3, total;
+            float per;
+            String grade;
+            Console.WriteLine("enter 3subject marks.");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            n3 = Convert.ToInt32(Console.ReadLine());
+            total = n1 + n2 + n3;
+            per = (float)(total / 300.0f) * 100.0f;
+            Console.WriteLine("total = " + total);
+            Console.WriteLine("per = " + per);
+            if (per >= 75)
+            {
+                grade = "distinction";
+            }
+            else if (per >= 60 && per < 75)
+            {
+                grade = "first";
+            }
+            else
+            {
+                grade = "fail";
+            }
+
+
+            Console.WriteLine("grede " + grade);
+
+            Console.ReadLine();
+        }
+    }
+}
