34bf5fc9ba755158910e980f3b31821a30a16743
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-14 23:56:55 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Enum
+namespace Example
+
 {
     class Program
     {
-        enum grade { dist = 90, first = 60, second = 40 }
+
         public static void Main()
         {
-            //write a program to accept character from the user 
-            grade gr;
+            long longValue = 1234567L;
+            int intValue = (int)longValue;
+            Console.WriteLine("(int) {0} = {1}", longValue, intValue);
 
-            gr = grade.first;
-            Console.WriteLine("grade  = " + gr);
-            Console.WriteLine("grade value = " + Convert.ToInt32(gr));
+            Console.ReadLine();
+
 
         }
+
+
     }
 }
