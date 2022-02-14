e407d1cfadb106fc2693cf4aa2420fc6f85ffbc6
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-14 23:53:22 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Character
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            char ch;
-            Console.WriteLine("enter character");
-            ch = Convert.ToChar(Console.Read());
-            Console.WriteLine("ch = " + ch);
-
-        }
-    }
-}
-
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
