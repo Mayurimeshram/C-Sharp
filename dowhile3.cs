75ab8c9908c5262d1d33240d0e635601eb0f117f
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 23:07:50 +0530

    Add files via upload

---
+++ dowhile3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine("value of i: {0}", i);
+
+                int j = i;
+                i++;
+                do
+                {
+                    Console.WriteLine("value of j : {0}", j);
+                    j++;
+                }
+                while (j < 2);
+            }
+            while (i < 2);
+        }
+    }
+}
