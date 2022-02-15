d52ec86358257106793b012390139bb25c554198
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 11:29:53 +0530

    Add files via upload

---
+++ assigment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept 3 no. and display addtion of 3 no.
+            int num1, num2,num3, res;
+            Console.WriteLine("enter number1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number3");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            res = num1 + num2+num3;
+            Console.WriteLine("addition = " + res);
+            Console.ReadLine();
+        }
+    }
+}
