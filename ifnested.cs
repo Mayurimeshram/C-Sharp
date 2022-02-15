f13192414ba6066c131460ff23fc711439a750fc
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 23:10:40 +0530

    Add files via upload

---
+++ ifnested.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            int number = 2;
+            if (number < 5)
+            {
+                Console.WriteLine("{0} is less than 5", number);
+            }
+            Console.WriteLine("this statement is always executed.");
+
+        }
+    }
+}
