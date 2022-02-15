0e3b50e6732e1b67082d57b73d9e3ebd04f36e2a
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 11:20:03 +0530

    Add files via upload

---
+++ char.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace character
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            char b = 'M';
+            Console.WriteLine(" value " + b);
+            Console.WriteLine("enter value ");
+            b = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("value = " + b);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
