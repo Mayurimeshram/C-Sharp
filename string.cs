420018926e2be602ff08fcbc9121ed60e48309c2
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 11:36:58 +0530

    Add files via upload

---
+++ string.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+
+        public static void Main()
+        {
+            string b = "mayuri";
+            Console.WriteLine("value" + b);
+            Console.WriteLine("enter value");
+            b = Console.ReadLine();
+            Console.WriteLine("value =" + b);
+            Console.ReadLine();
+        }
+    }
+}
