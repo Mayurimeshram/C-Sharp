bec1f926d64625425bcdde799898b0eb40dda693
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 14:49:32 +0530

    Add files via upload

---
+++ nest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // wap to print no from 1 to 10 using while loop
+            int i, j;
+            for (i = 1; i < 10; i++)
+            {
+                for (j = 1; j < i; j++)
+                {
+                    Console.Write(j + "\t");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
