0d7ce5cefdff6ba87855762fc00706c6f361d3ab
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 14:45:01 +0530

    Add files via upload

---
+++ foreach.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[3]; 
+            arr[0] = 11;
+            arr[1] = 22;
+            arr[2] = 33;
+            //using for loop 
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("arr = " + arr[i]);
+            }
+            //for each loop
+            //syntax foreach (datatype variable in arrayname) 
+            foreach (int i in arr)
+            {
+                Console.WriteLine("arr = " + i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
