3c43ec9dd535e3be6de5ede200b961bc185e915a
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 11:39:48 +0530

    Add files via upload

---
+++ swich.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            
+            int n1, n2, res = 0;
+            char op;
+            Console.WriteLine("Enter 2 no");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter operator");
+            op = Convert.ToChar(Console.ReadLine());
+            switch (op)
+            {
+                case '+':
+                    res = n1 + n2;
+                    break;
+                case '-':
+                    res = n1 - n2;
+                    break;
+                case '*':
+                    res = n1 * n2;
+                    break;
+                default:
+                    Console.WriteLine("invalid");
+                    break;
+
+            }
+
+            Console.WriteLine("res " + res);
+            Console.ReadLine();
+        }
+    }
+}
