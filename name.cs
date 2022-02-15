7df64985c0bfcd0608267cbf8d454be2fed0db67
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 11:35:57 +0530

    Add files via upload

---
+++ name.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept radius and print area and circ of circle
+            String name;
+            Console.WriteLine("enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("your name is " + name);
+            Console.ReadLine();
+        }
+    }
+}
+
