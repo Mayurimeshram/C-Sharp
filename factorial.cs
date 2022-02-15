70eff614c9d897f39bbf8fdde4a8f65ca5a56c01
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 14:43:11 +0530

    Add files via upload

---
+++ factorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void factorial(int num)
+        {
+            int fact = 1;
+            while (num > 0)
+            {
+                fact = fact * num;
+                num--;
+            }
+
+            Console.WriteLine("fact = " + fact);
+        }
+
+
+        static void Main(string[] args)
+        {
+            int num = 7;
+            factorial(num);
+            Console.ReadLine();
+        }
+    }
+}
