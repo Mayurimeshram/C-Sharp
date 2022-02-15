361af7198284fac3eac83cb1d9b3eb575f16dd32
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 11:30:32 +0530

    Add files via upload

---
+++ consoleapp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class program
+    {
+        static void main()
+        {
+            int n1, n2,result;
+            Console.WriteLine("enter number");
+               convert.twointConsole.ReadLine()
+        }
+    }
+}
