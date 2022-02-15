379c18e719c438909b2566fead271d2cb9c5f9e5
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 11:31:42 +0530

    Add files via upload

---
+++ console.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleApp1
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            console.writeline("hello")
+                console.Readline();
+        }
+    }
+}
