b0bb71f1f82dcc4c39625c6b6fb54f1e49535a7a
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 21:24:50 +0530

    Add files via upload

---
+++ while2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine("i ={0}", i);
+                i++;
+
+                if (i > 10)
+                    break;
+            }
+        }
+    }
+}
