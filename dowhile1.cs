e43f8b73cf9e00ebb56625001c8b9472c5d2e354
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 23:04:39 +0530

    Add files via upload

---
+++ dowhile1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("i ={0}", i);
+                i++;
+            }
+            while (i < 5);
+        }
+
+    }
+}
