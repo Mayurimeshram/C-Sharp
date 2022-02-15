4d3d2fa148a5bc2582006c04accb21cf3948983a
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 14:52:10 +0530

    Add files via upload

---
+++ table2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+        static int factorial(int num)
+        {
+            int fact = 1;
+
+            while (num > 0)
+            {
+                fact = fact * num;
+                num--;
+            }
+            return fact;
+
+
+        }
+        static void Main(string[] args)
+        {
+            int num = 5;
+            int result;
+            result = factorial(num);
+            Console.WriteLine("fact = {0}", result);
+            Console.ReadLine();
+
+        }
+    }
+}
