4a6182b625f5ae4812fabce19ec498ae2f0d8d60
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-15 11:38:48 +0530

    Add files via upload

---
+++ student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student2
+{
+    class Program
+    {
+        struct student
+        {
+            public int rno;
+            public String name;
+            public String course;
+        }
+        public static void Main()
+        {
+            student s1;
+            Console.WriteLine("enter rno");
+            s1.rno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter name");
+            s1.name = Console.ReadLine();
+            s1.course = Console.ReadLine();
+
+            Console.WriteLine("rno " + s1.rno);
+            Console.WriteLine("name " + s1.name);
+            Console.WriteLine("course " + s1.course);
+
+        }
+    }
+}
