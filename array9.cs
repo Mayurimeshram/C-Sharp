0f4538f87939f4f15efbd46f884794edff88bd37
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-17 10:09:34 +0530

    Add files via upload

---
+++ array9.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            long[] arr = new long[4] { 0, 1, 2, 3 };
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write("\t " + arr[j]);
+            }
+            Console.WriteLine();
+
+
+        }
+
+    }
+}
+
