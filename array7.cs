1ec228f52136b627f9128d32547c86ef14ed09e0
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-17 10:08:10 +0530

    Add files via upload

---
+++ array7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("enter marks");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("marks " + arr[i]);
+            }
+        }
+    }
+}
+      
