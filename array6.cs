eef10475a9768757c8563d380672ce72a18131b3
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-17 10:06:55 +0530

    Add files via upload

---
+++ array6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static long Add(int x, long y)
+        {
+            return x + y;
+        }
+        static void Main()
+        {
+            Console.WriteLine("addition of 2 no. " + Add(1, 2));
+            Console.WriteLine("addition of  3 no " + Add(1, 2L));
+        }
+
+    }
+}
+
