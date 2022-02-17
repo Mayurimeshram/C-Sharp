f8dfecee2fe56f997a11e35fc3c340bcccc6e002
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-17 10:04:45 +0530

    Add files via upload

---
+++ array3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void AddOne(out int x)
+        {
+            x = 5;
+            x++; 
+                 //  Console.WriteLine("value inside addone method is " + x); //6
+        }
+        static void Main()
+        {
+            int roi;
+            AddOne(out roi);
+            Console.WriteLine("rate of interest  " + roi); 
+
+        }
+    }
+}
+
