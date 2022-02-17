668c61758d2e66bef31bd48613322f1ef4d3c7b4
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-17 10:11:19 +0530

    Add files via upload

---
+++ array11.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] grid = { { 5, 4, 3 }, { 2, 1, 0 } };
+
+
+            
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("\t " + grid[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+
+    }
+}
