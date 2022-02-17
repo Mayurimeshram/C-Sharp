b5bd89882e4201402274ab7e067962ba112c8c20
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-17 10:05:25 +0530

    Add files via upload

---
+++ array4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static long AddList(params long[] v)
+        {
+            long total, i;
+            for (i = 0, total = 0; i < v.Length; i++)
+            {
+                total += v[i];
+            }
+            return total;
+        }
+        static void Main()
+        {
+            long x = AddList(1, 2, 3, 4, 5);
+            Console.WriteLine("value of x = " + x);
+        }
+
+    }
+}
+
+
