75238df9aaef317cb45052ee1677b726c917fa59
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-17 10:08:47 +0530

    Add files via upload

---
+++ array8.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] arr = new int[3, 3]; //2 dimension array
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("enter marks");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            //displaying the array elements
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++) 
+                {
+                    Console.Write("\t " + arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
+
+
+
