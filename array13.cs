48cfc8e4add54a9957a183edd2d1a2a14c2cfcc5
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-17 10:13:11 +0530

    Add files via upload

---
+++ array13.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            long[] row = new long[4] { 1, 2, 3, 4 };
+            long[] copy = row;
+
+            row[0]++;
+            long value = copy[0];
+            Console.WriteLine(value + "\n");
+
+
+            //displaying the array elements
+            for (int i = 0; i < copy.Length; i++)
+            {
+
+                Console.WriteLine(" " + copy[i]);
+
+            }
+
+
+        }
+
+    }
+}
