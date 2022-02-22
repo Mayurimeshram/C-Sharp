04aa565e67fb5a3cbb7504df94c187c611205a5e
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-23 00:10:55 +0530

    Add files via upload

---
+++ 213Ex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace AccessSpecifies
+{
+    class ProtectedTest
+    {
+        protected string name = "mayuri";
+        public void Msg(string msg)
+        {
+            Console.WriteLine("Hello" + msg);
+
+        }
+
+    }
+       class Program:ProtectedTest
+    {
+        static void Main (string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine("Hello" + program.name);
+            program.Msg("Mayuri");
+
+
+
+        }
+            
+    }
+}
