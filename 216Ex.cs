170bdfc4d8871f0455b4ffa9636835eeee34c919
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-23 00:13:14 +0530

    Add files via upload

---
+++ 216Ex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace AccessSpecifiers
+{  
+ ..       class PrivateTest
+    {
+        private string name = "Shantosh Kumar";
+        private void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateTest privateTest = new PrivateTest();
+            
+            Console.WriteLine("Hello " + privateTest.name);
+            
+            privateTest.Msg("Peter Decosta");
+        }
+    }
+}
