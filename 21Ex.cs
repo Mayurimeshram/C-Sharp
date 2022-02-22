adf3ece4407a63c83f987fc34ccf9b51a5c19541
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-23 00:09:29 +0530

    Add files via upload

---
+++ 21Ex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace AccessSpecifiers
+{
+    class PublicTest
+    {
+        public String name = "Mayuri Meshram";
+        public void msg(String msg)
+        {
+            Console.WriteLine("Hello " + msg);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PublicTest publicTest = new PublicTest();
+
+            //Accesing Public Variable
+            Console.WriteLine("Hello" + publicTest. name);
+                     //Acessing Public Function
+                  publicTest.msg("PeterDecosta");
+        }
+    }
+}
+
