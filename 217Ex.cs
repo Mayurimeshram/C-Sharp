2cb2823c5a816291eb33e0f018da8b195e4c54ef
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-23 00:13:57 +0530

    Add files via upload

---
+++ 217Ex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace AccessSpecifiers
+{
+    class Program
+    {
+        private string name = "Mayuri";
+        private void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            
+            Console.WriteLine("Hello " + program.name);
+           
+            program.Msg("Mona");
+        }
+    }
+}
