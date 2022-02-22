0253c8e9f379e87a56a680c66424c469ff6bb6e8
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-22 20:24:09 +0530

    Add files via upload

---
+++ Int1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Employee
+{
+    public float salary = 4000;
+
+}
+
+public class Programmer: Employee
+    {
+    public float bonus = 10000;
+
+
+}
+class Testinheritance
+{
+    public static void Main(string[] args)
+    {
+        Programmer p1 = new Programmer();
+        Console.WriteLine("Salary:" + p1.salary);
+
+        Console.WriteLine("Bonus:" + p1.bonus);
+    }
+}
