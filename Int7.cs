205bfb10ea3be4e9251f25023207c6d90b5db399
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-23 00:03:10 +0530

    Add files via upload

---
+++ Int7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+public class Person
+    {
+        string name;
+        string adrress;
+        protected void getPersonData(string name, string adrress)
+        {
+            this.name = name;
+            this.adrress = adrress;
+
+        }
+          protected void di
+    }
