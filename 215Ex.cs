d7ac9446e6eb78746f12b0b7b06e9cfb076fbeeb
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-23 00:12:31 +0530

    Add files via upload

---
+++ 215Ex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace AccessSpecifier
+{
+    class InternalTest
+    {
+        protected internal string name = "Mayuri";
+        protected internal void Msg(string msg)
+        {
+            Console.WriteLine("Hello" + msg);
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            InternalTest internalTest = new InternalTest();
+            Console.WriteLine("Hello" + internalTest.name);
+            internalTest.Msg("Mona");
+
+
+        }
+    }
+}
