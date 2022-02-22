a61ade3764bb4a732825c8cf376a05707834f282
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-23 00:00:47 +0530

    Add files via upload

---
+++ Int2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class person
+    {
+        public string name;
+        public string address;
+    }
+    public class student: person
+    {
+        public int rollno;
+        public int masks;
+    }
+    class Testinheritance
+    {
+        public static void Main(string[] args)
+        {
+            student p1 = new student();
+            p1.rollno = 129;
+            p1.name = "Mayuri";
+            p1.address = "Mansar";
+            p1.masks = 88;
+
+            Console.WriteLine("rollno:" + p1.rollno);
+            Console.WriteLine("name:" + p1.name);
+            Console.WriteLine("address:" + p1.address);
+            Console.WriteLine("masks:" + p1.masks);
+
+        }
+    }
+}
