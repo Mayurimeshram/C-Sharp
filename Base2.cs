8c81553050e94d6aa73488c321a3785742ade986
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-23 12:03:18 +0530

    Add files via upload

---
+++ Base2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public void eat()
+        {
+            base.eat(); //this will class base class method eat
+            Console.WriteLine("eating bread...");
+        }
+
+    }
+    public class TestBase
+    {
+        public static void Main()
+        {
+            Dog g = new Dog();
+            g.eat();
+        }
+    }
+
+}
+
