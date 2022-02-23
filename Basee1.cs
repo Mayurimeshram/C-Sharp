9467b8ba65f2ffb24221f862651ff6351e2b32fb
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-23 12:02:41 +0530

    Add files via upload

---
+++ Basee1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Animal
+{
+    public string color = "white";
+}
+  public class Dog: Animal
+{
+    string color = "black";
+    public void showcolor()
+    {
+        Console.WriteLine(base.color);
+        Console.WriteLine(color);
+    }
+     public class TestBase
+    {
+        public static void Main()
+        {
+            Dog d = new Dog();
+            d.showcolor();
+        }
+
+
+    }
+}
