c7818d1a84111e18c73acca9577fa1472f4d0398
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-25 01:22:30 +0530

    Add files via upload

---
+++ Interface1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public interface Drawable
+{
+    void Draw();
+}
+ public class Rectangle: Drawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing Reactangle.....");
+    }
+}
+ public class Circle: Drawable
+
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing circle....");
+    }
+}
+ public class TestInterface
+{
+    public static void Main()
+    {
+        Drawable d;
+        d = new Rectangle();
+        d.Draw();
+        d = new Circle();
+        d.Draw();
+
+    }
+}
