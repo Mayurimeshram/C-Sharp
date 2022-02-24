2ed9259164436decf30d4e2c0ef7c51bc6bfddd0
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-24 12:56:03 +0530

    Add files via upload

---
+++ Runtime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Animal
+{
+    public virtual void eat()
+    {
+        Console.WriteLine("eating.....");
+
+    }
+}
+  public  class Dog: Animal
+{
+    public override void eat()
+    {
+        Console.WriteLine("eating bread...");
+    }
+}
+public class Testploymorphism
+{
+    public static void Main()
+    {
+        Animal a = new Dog();
+        a.eat();
+    }
+
+    
+}
+
+
