7b6e5aceb7e7abdb7aeaa2732eadd7f039d1026e
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-24 13:07:45 +0530

    Add files via upload

---
+++ Overriding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Animal
+{
+    public virtual void eat()
+    {
+        Console.WriteLine("Eating.....");
+
+    }
+}
+  public class Dog: Animal
+{
+    public override void eat()
+    {
+        Console.WriteLine("Eating Bread.....");
+    }
+}
+   public class TestOverriding
+{
+    public static void Main()
+    {
+        Animal d = new Dog();
+        d.eat();
+
+    }
+}
