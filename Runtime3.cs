975c79a86cc91a8593f16c1c45a97559ec018e34
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-24 12:57:46 +0530

    Add files via upload

---
+++ Runtime3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+ public class Animal
+{
+    public string color = "White";
+
+}
+ public  class Dog: Animal
+{
+    public string color = "Black";
+}
+    public class Testsealed
+{
+    public static void Main()
+    {
+        Animal d = new Dog();
+        Console.WriteLine(d.color);
+    }
+}
