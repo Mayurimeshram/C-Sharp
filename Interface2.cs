010d18a7b3ac5e13bd8cb41e14b017a4a32c82cb
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-02-25 01:23:22 +0530

    Add files via upload

---
+++ Interface2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    interface Bank
+    {
+        string Deposit(int Actno, int Amt);
+        }
+    class Saving : Bank
+    {
+        int Actno;
+        int Bal = 2000;
+        public string Deposit(int Actno, int Amt)
+        {
+            this.Actno = Actno; ;
+            Bal = Bal + Amt;
+            return "deposited successfully successfully ,Bal is " + Bal;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b;
+            b = new Saving();
+            String str = b.Deposit(1, 200);
+            Console.WriteLine(str);
+
+        }
+    }
+
+}
