7ca08a3781a9cf0d41ea4485b77f21336d80ab07
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-04 12:55:11 +0530

    Add files via upload

---
+++ Arraylist2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add("11");
+            ar.Add("12");
+            ar.Add("13");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno" + obj);
+            }
+            Console.WriteLine("Capacity: {0}", ar.Capacity);
+            Console.WriteLine("Count:{0}", ar.Count);
+        }
+    }
+}
+
+
+                
