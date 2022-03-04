a6f7e1ed94ec1785c090888dafc024fa8cc6a213
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-04 12:54:00 +0530

    Add files via upload

---
+++ Arraylist1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add("mayuri");
+            ar.Add("mona");
+            ar.Add("mayu");
+            foreach (object obj in ar)
+            {
+                Console.WriteLine(" student name" + obj);
+            }
+        }
+    }
+}
