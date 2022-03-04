f63c391f64a6d6aae7809ce299439edb8c34a192
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-04 12:56:45 +0530

    Add files via upload

---
+++ Arraylist4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(19);
+            ar.Add(12);
+            ar.Add(13);
+            
+            Console.WriteLine("array list colection without sorting");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+            ar.Insert(2, 19);
+            Console.WriteLine("array list colection after adding element at 2 position");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+        }
+    }
+}
