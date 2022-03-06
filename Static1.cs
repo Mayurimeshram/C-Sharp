5c7289e3cf2a692123514c7b34cb7dd7b6ed8a62
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-07 00:32:57 +0530

    Add files via upload

---
+++ Static1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+ namespace S
+{
+    class Program
+    {
+        void display()
+        {
+            Console.WriteLine("hello");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.display();
+        }
+    }
+
+}
