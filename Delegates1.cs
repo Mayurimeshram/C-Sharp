71b81c23c55defdeb169b0970888947ec86b0186
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-06 23:44:44 +0530

    Add files via upload

---
+++ Delegates1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp7
+{
+
+
+
+    class Program
+    {
+        //declaring a delegate to hold reference to a method
+        delegate void del_display();
+        //creating a method
+        public void display()
+        {
+            Console.WriteLine("hello from display method");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //creating object of delegate
+            del_display d1 = new del_display(p.display);
+            //calling the delegate which in turns call the method display
+            d1();
+
+
+
+
+
+        }
+    }
+}
