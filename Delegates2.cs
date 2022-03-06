a1b11beefd6b84715128958f15eb26b0f37b935f
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-06 23:46:09 +0530

    Add files via upload

---
+++ Delegates2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp7
+{
+    public delegate int del_square(int n1);
+    class program
+    {
+        public int square(int n1)
+        {
+            return n1 * n1;
+        }
+        static void Main(string[] args)
+        {
+            program p = new program();
+            del_square d1 = new del_square(p.square);
+            int res = d1(4);
+            Console.WriteLine("square = " + res);
+
+
+        }
+    }
+}
