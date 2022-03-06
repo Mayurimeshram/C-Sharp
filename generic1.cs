a87e7bfd1c523c7963b822aaa72672cae546d5fb
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-07 00:00:42 +0530

    Add files via upload

---
+++ generic1.cs
@@ -0,0 +1,27 @@
+﻿List<string> langs = new List<string>();
+
+langs.Add("Java");
+langs.Add("C#");
+langs.Add("C");
+langs.Add("C++");
+langs.Add("Ruby");
+langs.Add("Javascript");
+
+Console.WriteLine(langs.Contains("C#"));
+
+Console.WriteLine(langs[1]);
+Console.WriteLine(langs[2]);
+
+langs.Remove("C#");
+langs.Remove("C");
+
+Console.WriteLine(langs.Contains("C#"));
+
+langs.Insert(4, "Haskell");
+
+langs.Sort();
+
+foreach (string lang in langs)
+{
+    Console.WriteLine(lang);
+}
