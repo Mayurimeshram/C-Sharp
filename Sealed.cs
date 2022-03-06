358b055ea8d60fbf06ac68bc9b953065b7ba7dfe
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-07 00:25:23 +0530

    Add files via upload

---
+++ Sealed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    sealed class student
+    {
+        public string name;
+    }
+
+    class parttime : student ///error will come
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
