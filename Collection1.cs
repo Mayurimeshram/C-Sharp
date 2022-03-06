5b493c099aa6da6920be4a34b4948bfffc675e76
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-06 23:41:50 +0530

    Add files via upload

---
+++ Collection1.cs
@@ -0,0 +1,25 @@
+﻿
+public class Customer
+{
+    public Customer(int id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
+
+    private int m_id;
+
+    public int ID
+    {
+        get { return m_id; }
+        set { m_id = value; }
+    }
+
+    private string m_name;
+
+    public string Name
+    {
+        get { return m_name; }
+        set { m_name = value; }
+    }
+}
