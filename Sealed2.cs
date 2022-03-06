8de00db4d957660e806efcff8c4ad7c89f08cbda
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-07 00:29:55 +0530

    Add files via upload

---
+++ Sealed2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class person
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine(" Hello From Person");
+
+        }
+    }
+    class student : person
+    {
+        public string name;
+        public sealed override void Display()
+        {
+            Console.WriteLine("hello");
+        }
+        class parttime : student
+        {
+            /*
+            public  override void display() //error since sealed
+            {
+                Console.WriteLine("hello");
+            }
+            */
+        }
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                parttime p = new parttime();
+                p.Display();
+            }
+
+        }
+    }
+}
+
+
+    
