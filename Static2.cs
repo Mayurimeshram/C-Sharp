381a6a185dc8e81f76e5ab12582e310daf163b2e
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2022-03-07 00:33:45 +0530

    Add files via upload

---
+++ Static2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+ namespace s
+{
+    class Program
+    {
+        static void display()
+        {
+            Console.WriteLine("hello");
+        }
+        static void Main(string[] args)
+        {
+
+            display(); //to access static method display instance is not required
+        }
+    }
+
+}
